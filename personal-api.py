65ef0949c54f60a2a0792c6df8c5dc853a8d4ccc
sanjaykumar3453 <sanjaykumart678@gmail.com>
2025-03-09 15:32:38 +0530

    Update personal-api.py

--- personal-api.py
+++ personal-api.py
@@ -1,4 +1,19 @@
-Update this file to add your code
-/name - Should give your name
-/register_number - Should give your register_number
-/department - Should give your department
+from flask import Flask
+
+app =Flask(__name__)
+
+@app.route("/")
+def name():
+    return "Sanjay kumar T"
+
+
+@app.route("/registerNumber")
+def register_number():
+    return "22IT043"
+
+@app.route("/department")
+def department():
+    return "IT(Information Technology)"
+
+if __name__ == "__main__":
+    app.run(debug=True)
